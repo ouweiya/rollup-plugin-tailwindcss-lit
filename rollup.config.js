13a2872212d45c00532a346961e1ae2fa8ae9a68
ouweiya <higogle1@gmail.com>
2023-06-10 01:10:46 +0800

    1.0.7

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import type { RollupOptions } from 'rollup';
+// import type { RollupOptions } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'rollup';
 
-const config: RollupOptions = {
+const config = defineConfig({
   input: ['src/index.ts'],
   output: {
     format: 'es',
@@ -22,6 +23,6 @@ const config: RollupOptions = {
   //   warn(warning);
   // },
   plugins: [typescript(), nodeResolve(), commonjs()],
-};
+});
 
 export default config;
