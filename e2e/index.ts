08f05cf6ec6097ce38a56c7a75f510c336ccefb3
ouweiya <higogle1@gmail.com>
2023-10-31 02:40:47 +0800

    1.2.4

--- e2e/index.ts
+++ e2e/index.ts
@@ -1,21 +1,15 @@
 
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import style from './index.css';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-    static styles = [style, 
-        css`
-        {
-            color: green;
-            @apply bg-pink-500;
-        }`,
-        css`
-        h3 {
-            @apply bg-red-500;
-        }`
-        ];
+    static styles = css`
+            p {
+                color: green;
+                @apply bg-red-200;
+            }`
+        ;
     protected render() {
         return html`<p class='text-blue-600'>I am green!</p>`;
     }
